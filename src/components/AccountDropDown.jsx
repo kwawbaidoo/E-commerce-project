a1d5904bc616ad10f8346658feb9b1796bf963f1
kwawbaidoo <kwaw.baidoo@gmail.com>
2024-07-18 12:35:23 +0000

    frontend

--- src/components/AccountDropDown.jsx
+++ src/components/AccountDropDown.jsx
@@ -20,15 +20,15 @@ const AccountDropDown = ({ isOpen, onClose }) => {
 
   return (
     <div className="fixed inset-0 bg-transparent bg-opacity-50 flex justify-center items-center z-50 modal-overlay">
-      <div className="bg-customgray  rounded-lg shadow-lg p-6 w-52 -top-60 left-[500px] relative">
+      <div  className="bg-customgray  rounded-lg shadow-lg p-6 w-52 -top-60 left-[500px] relative">
         <button onClick={onClose} className="absolute top-0 right-0 mt-4 mr-4 text-gray-500  modal-overlay">
           &#x2715;
         </button>
         <em className="text-md font-semibold  text-gray-500 ">Profile</em>
         <ul className='flex flex-col mt-4'>
-          <Link to="/profileEdit" className="mb-2">
+          <Link onClick={()=>onClose()} to="/profileEdit" className="mb-2 modal-overlay ">
             
-            <button onClick={onClose} className="text-gray-500 hover:underline flex items-center gap-2">
+            <button  className="text-gray-500 hover:underline flex items-center gap-2 ">
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none"><path d="M12 15a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" stroke="#DB4444" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"></path><path d="M2 12.88v-1.76c0-1.04.85-1.9 1.9-1.9 1.81 0 2.55-1.28 1.64-2.85-.52-.9-.21-2.07.7-2.59l1.73-.99c.79-.47 1.81-.19 2.28.6l.11.19c.9 1.57 2.38 1.57 3.29 0l.11-.19c.47-.79 1.49-1.07 2.28-.6l1.73.99c.91.52 1.22 1.69.7 2.59-.91 1.57-.17 2.85 1.64 2.85 1.04 0 1.9.85 1.9 1.9v1.76c0 1.04-.85 1.9-1.9 1.9-1.81 0-2.55 1.28-1.64 2.85.52.91.21 2.07-.7 2.59l-1.73.99c-.79.47-1.81.19-2.28-.6l-.11-.19c-.9-1.57-2.38-1.57-3.29 0l-.11.19c-.47.79-1.49 1.07-2.28.6l-1.73-.99a1.899 1.899 0 0 1-.7-2.59c.91-1.57.17-2.85-1.64-2.85-1.05 0-1.9-.86-1.9-1.9Z" stroke="#DB4444" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"></path></svg>
                 Manage Account</button>
           </Link>
